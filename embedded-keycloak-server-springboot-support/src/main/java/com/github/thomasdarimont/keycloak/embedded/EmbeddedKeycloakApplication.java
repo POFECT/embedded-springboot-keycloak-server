e30caba26fdeceb25309b3466a87f980ae4f99f3
Yoon Hyejin <99111030+yyhjin@users.noreply.github.com>
2024-01-15 20:17:10 +0900

    Update EmbeddedKeycloakApplication.java

--- embedded-keycloak-server-springboot-support/src/main/java/com/github/thomasdarimont/keycloak/embedded/EmbeddedKeycloakApplication.java
+++ embedded-keycloak-server-springboot-support/src/main/java/com/github/thomasdarimont/keycloak/embedded/EmbeddedKeycloakApplication.java
@@ -4,12 +4,14 @@ import com.github.thomasdarimont.keycloak.embedded.KeycloakCustomProperties.Admi
 import com.github.thomasdarimont.keycloak.embedded.support.SpringBootConfigProvider;
 import lombok.extern.slf4j.Slf4j;
 import org.keycloak.Config;
+import org.keycloak.common.enums.SslRequired;
 import org.keycloak.exportimport.ExportImportConfig;
 import org.keycloak.exportimport.ExportImportManager;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakTransactionManager;
 import org.keycloak.services.ServicesLogger;
 import org.keycloak.services.managers.ApplianceBootstrap;
+import org.keycloak.services.managers.RealmManager;
 import org.keycloak.services.resources.KeycloakApplication;
 import org.springframework.core.io.Resource;
 import org.springframework.util.StringUtils;
